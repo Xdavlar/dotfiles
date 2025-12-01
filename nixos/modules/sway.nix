5bd3d5154e96bbf1462f5c1ab0eecf4f803fd459
Erik Carlsson <erikcarlsson90@gmail.com>
2025-12-01 22:30:25 +0100

    Removed a lot of attempted fixes from the slow startup install

--- nixos/modules/sway.nix
+++ nixos/modules/sway.nix
@@ -15,18 +15,17 @@
       slurp # screenshot functionality
       wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
       mako # notification system developed by swaywm maintainer
-      rofi-wayland-unwrapped # launcher, replaces wmenu
+      rofi-unwrapped # launcher, replaces wmenu
     ];
+
+    # Enable the X11 windowing system.
+    # You can disable this if you're only using the Wayland session.
+    services.xserver.enable = true;
 
     # enable Sway window manager
     programs.sway = {
       enable = true;
       wrapperFeatures.gtk = true;
-    };
-
-    environment.variables = {
-      GTK_USE_PORTAL = 0;
-      GST_PLUGIN_SYSTEM_PATH_1_0 = ["/run/current-system/sw/lib/gstreamer-1.0"];
     };
 
     programs.bash.shellAliases = {
@@ -41,22 +40,6 @@
           user = "erik";
         };
         default_session = initial_session;
-      };
-    };
-
-    xdg.portal = {
-      enable = true;
-      wlr.enable = true;
-      extraPortals = with pkgs; [
-        xdg-desktop-portal-wlr
-        xdg-desktop-portal-gtk
-      ];
-      config = {
-        sway = {
-          default = lib.mkForce ["wlr" "gtk"];
-          "org.freedesktop.impl.portal.Screenshot" = ["wlr"];
-          "org.freedesktop.impl.portal.ScreenCast" = ["wlr"];
-        };
       };
     };
   };
