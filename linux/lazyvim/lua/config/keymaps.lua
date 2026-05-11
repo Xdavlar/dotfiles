81b5f7afa4965adaf5d5687769c9fb4d455a1f22
Erik Carlsson <erikcarlsson90@gmail.com>
2026-05-11 21:58:38 +0200

    Updated nvim configuration

--- linux/lazyvim/lua/config/keymaps.lua
+++ linux/lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<C-k>", 'a[]()<Left><Left><Left>', { desc = "Insert markdown link" })
+vim.keymap.set("v", "<C-k>", 'c[<C-r>"]()<Esc>hi', { desc = "Wrap in markdown link" })
