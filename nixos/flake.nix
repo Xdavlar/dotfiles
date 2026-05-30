7bcf8109f093359da59064e5f3481482e5a4c4a9
Erik Carlsson <erikcarlsson90@gmail.com>
2026-05-30 22:05:38 +0200

    Updated flake files after update

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,13 +2,13 @@
   description = "Erik's NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixos-vscode-server.url = "github:nix-community/nixos-vscode-server";
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
