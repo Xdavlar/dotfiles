103a115cc32d9b6fb6c05c74e2217bc9f0342c01
Erik Carlsson <erikcarlsson90@gmail.com>
2026-05-30 22:01:39 +0200

    Added zoxide

--- nixos/hm/users/erik/default.nix
+++ nixos/hm/users/erik/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ../../modules/shell/bash.nix
     ../../modules/shell/git.nix
+    ../../modules/shell/zoxide.nix
     ../../modules/programs/vim.nix
     ../../modules/programs/nano.nix
   ];
