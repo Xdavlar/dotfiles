626aa77fe66cbadfd5d3f93ff36b3bad7233fa7a
Erik Carlsson <Erik Carlsson>
2026-06-02 20:29:17 +0000

    Added claude code

--- nixos/hm/users/erik/nixos-vm-docker.nix
+++ nixos/hm/users/erik/nixos-vm-docker.nix
@@ -1,9 +1,10 @@
-{pkgs, ...}: {
+{pkgs, pkgs-unstable, ...}: {
   imports = [./default.nix];
 
   home.stateVersion = "23.11";
 
   home.packages = with pkgs; [
+    pkgs-unstable.claude-code
     atuin
     docker-compose
     libnotify
