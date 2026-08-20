8d6d8ed1bd451aa55ca01d61659ad1e96f17aef5
Erik Carlsson <erikcarlsson90@gmail.com>
2026-08-20 22:28:23 +0200

    fix: Changed so that the powerbutton has to be long pressed
    * This is to stop me accidentaly shutting down the computer

--- nixos/hosts/erik-pc/configuration.nix
+++ nixos/hosts/erik-pc/configuration.nix
@@ -70,6 +70,10 @@
           gnome.gnome-keyring.enable = true;
           tailscale.enable = true;
           openssh.enable = true;
+          logind.setting.Login = {
+            HandlePowerKey = "ignore";
+            HandlePowerKeyLongPress = "poweroff";
+          };
         };
 
         users.users.erik = {
